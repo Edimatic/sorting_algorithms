77f2e2006c01609a668fedf27cc43fe6c5786fad
Edimatic <ediomiudofia@gmail.com>
2024-02-16 10:02:37 -0800

    Insertion sort

--- sort.h
+++ sort.h
@@ -37,6 +37,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
+
 /* Algoritms functions for the sorting */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
