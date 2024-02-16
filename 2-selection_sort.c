550d711b172819a70173af7d11b68e79733ad2d2
Edimatic <ediomiudofia@gmail.com>
2024-02-16 10:25:06 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order using
+ *                  the Selection sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+		size_t i, j, min_idx;
+		int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+	min_idx = i;
+
+	for (j = i + 1; j < size; j++)
+	{
+	if (array[j] < array[min_idx])
+		min_idx = j;
+	}
+
+	if (min_idx != i)
+	{
+	temp = array[i];
+	array[i] = array[min_idx];
+	array[min_idx] = temp;
+
+	print_array(array, size);
+		}
+	}
+}
+
