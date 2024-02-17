5a719a58387d1b5a21596746a83567bdb988cddf
Edimatic <ediomiudofia@gmail.com>
2024-02-17 06:11:19 -0800

    heap sort work

--- sort.h
+++ sort.h
@@ -56,6 +56,10 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+
 /* Algoritms functions for the sorting */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
