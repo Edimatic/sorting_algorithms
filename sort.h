585f95ea9d6512ff56952d9b2e1a317dddd84e92
Edimatic <ediomiudofia@gmail.com>
2024-02-17 04:34:15 -0800

    Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -46,7 +46,11 @@ size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size);
 void quicksort(int *array, ssize_t low, ssize_t high, size_t size);
 void quick_sort(int *array, size_t size);
 
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
+void swap_node_ahead(listint_t **list, listint_t **eddi, listint_t **node2);
+void swap_node_behind(listint_t **list, listint_t **eddi, listint_t **node2);
+void cocktail_sort_list(listint_t **list);
+
 
 /* Algoritms functions for the sorting */
 void bubble_sort(int *array, size_t size);
@@ -54,7 +58,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
+
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
